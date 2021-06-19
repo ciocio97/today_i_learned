bed72062635f890597381d97f16634985c145871
ciocio97 <80025242+ciocio97@users.noreply.github.com>
2021-06-19 23:09:47 +0900

    programmers Lv1

--- Coding-test/01. array1.js
+++ Coding-test/01. array1.js
@@ -43,7 +43,7 @@
 // 1. 2개의 parm 안의 배열 값을 같은 index에 있는 것끼리 곱하고,
 //    총합을 구한다. -> for문 or reduce
 
-// 풀이1
+// 풀이 1
 {
     function solution(a, b) {
         let answer = 0;
@@ -54,13 +54,13 @@
         return answer
     }
 }
-// 풀이2
+// 풀이 2
 {
     function solution(a, b) {
         return a.reduce((prvNum, currNum, index) => prvNum + currNum * b[index], 0);
     }
 }
-// 풀이3
+// 풀이 3
 {
     function solution(a, b) {
         const answer = a.reduce((prvNum, currNum, index) => {
