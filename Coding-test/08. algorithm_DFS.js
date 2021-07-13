d626698eec063c0e62e313b853a197309d91a74a
ciocio97 <80025242+ciocio97@users.noreply.github.com>
2021-07-13 12:46:39 +0900

    Recursion

--- Coding-test/08. algorithm_DFS.js
+++ Coding-test/08. algorithm_DFS.js
@@ -24,21 +24,34 @@
 
 // 풀이 2
 
-// * 재귀함수 * (Recursion)
+// * 재귀 함수 * (Recursion)
+{
+    function f(n){                     
+        if(n <= 1){                        // 재귀 함수는 100부터 순회한 다음, 호출 스택에 쌓여있는 값을 처리해 나간다.
+            return 1;                      // Stack은 LIFO (Last In First Out)방식.              
+        }                                  // 이때, 스택에 제일 마지막으로 반환된 1부터 제일 처음 반환된 100까지 차례대로 꺼내가며 더한다.
+        return n + f(n - 1);          
+    }
+    console.log(f(100));
+}
+
 // (내가 생각하는 그) Tree 구조
 {
     const numbers = [1,1,1,1,1];
-    recur(0,0);
 
-    function recur(idx, sum){
+    function recur(idx, sum){               // *** 재귀함수는 '종료 조건'이 없으면 무한 반복한다 = Maximum call stack size exceeded
         recur(idx+1, sum + numbers[idx]);   // 입력값 recur(0,0)이 2개가 되고, 4개가 되고, 8개, 16개, 32개가 되어가는 현장을 보고 계십니다
         recur(idx+1, sum - numbers[idx]);   // inx를 다 돌고, sum이 target을 만족할 땐 멈추고 경우의 수를 반환하는 미덕을 보여줘야겠죠 
     }
+    recur(0,0);                                       
 }
-// 풀이 2 완성 ㅎㅎ
+// 풀이 2 완성 
 {
+    var numbers = [1,1,1,1,1];
+    var target = 3;
+    var answer = 0;
+    
     function solution(numbers, target) {
-        var answer = 0;
         function recur(idx, sum){
             if(idx === numbers.length){
                 if(sum === target){
@@ -49,7 +62,8 @@
             recur(idx+1, sum + numbers[idx]);
             recur(idx+1, sum - numbers[idx]);
         }
-        recur(0,0);   // recur()함수 앞쪽에 선언하면 안먹힘 why ??
+        recur(0,0);                         // recur()함수 앞쪽에 선언했을 때, recur(0,0) is not defined 뜬다.
         return answer;
     }
+    console.log(solution(numbers, target));
 }
