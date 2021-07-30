51f0d2a685ec687f026a4ab7ba02c35c697a0a5e
ciocio97 <tmddusleo@icloud.com>
2021-07-30 18:30:24 +0900

    21.08.29 CLI

---
+++ Education_in_CodeStates/im-sprint-cli-practice-master/getListMultiplesOfTwo.js
@@ -0,0 +1,8 @@
+const { range } = require('range'); // range 모듈을 불러옵니다
+
+function getListMultiplesOfTwo(upTo) {
+  // TODO
+  return {range}.range(2, upTo+1, 2);
+}
+
+module.exports = getListMultiplesOfTwo;
