30657f264df678ae957474bfa4f41785d4542381
ciocio97 <tmddusleo@icloud.com>
2021-08-09 15:22:50 +0900

    전화번호 정규표현식 추가

--- Education_in_CodeStates/21.08.06 validation-check/src/validator.js
+++ Education_in_CodeStates/21.08.06 validation-check/src/validator.js
@@ -1,8 +1,41 @@
-// 여기에는 유용한 유효성 검증 함수가 있습니다
+
+let elInputId = document.querySelector('#username');
+let successMessege = document.querySelector('.success-message');
+let failureMessege = document.querySelector('.failure-message');
+
+elInputId.onkeyup = function() {
+  if(onlyNumberAndEnglish(elInputId.value)) {
+    failureMessege.classList.add('hide');
+    successMessege.classList.remove('hide');
+  }else {
+    if(elInputId.value.length === 0){
+      failureMessege.classList.add('hide');
+      successMessege.classList.add('hide');
+    }else {
+      failureMessege.classList.remove('hide');
+      successMessege.classList.add('hide');
+    }
+  }
+}
 
 // [유효성 검증 함수]: 영어 또는 숫자만 가능
 function onlyNumberAndEnglish(str) {
-  return /^[A-Za-z][A-Za-z0-9]*$/.test(str);  // *왜 쓰는거지
+  return /^[A-Za-z][A-Za-z0-9]{3,}$/.test(str);  // *왜 쓰는거지
+}
+
+// 패스워드 검사
+let failurePassword = document.querySelector('.failure-password');
+
+password.onkeyup = function() {
+  if(strongPassword(password.value)){
+    failurePassword.classList.add('hide');
+  }else {
+    if(password.value.length === 0){
+      failurePassword.classList.add('hide');
+    }else {
+      failurePassword.classList.remove('hide');  
+    }
+  }
 }
 
 // [유효성 검증 함수]: 최소 8자 이상하면서, 알파벳과 숫자 및 특수문자(@$!%*#?&) 는 하나 이상 포함
@@ -10,12 +43,27 @@ function strongPassword(str) {
   return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(str);
 }
 
-// 주석을 제거하고, 유효성 검증 함수를 테스트 해보세요
+// 휴대폰 번호 검사
+let phoneNumber = document.querySelector('#phone-number');
+let successNumber = document.querySelector('.success-number');
+let failureNumber = document.querySelector('.failure-number');
 
-console.log('`codestates`는 영어만 포함하므로', onlyNumberAndEnglish('codestates'))
-console.log('`김coding`은 영어 외의 다른 글자도 포함하므로', onlyNumberAndEnglish('김coding'))
-console.log('`김코딩`은 영어 외의 다른 글자도 포함하므로', onlyNumberAndEnglish('김코딩'))
-console.log('`김코딩`은 영어 외의 다른 글자도 포함하므로', onlyNumberAndEnglish(''))
-console.log('`qwerty`는 충분히 강력하지 않은 암호이므로', strongPassword('qwerty'))
-console.log('`q1w2e3r4`는 특수문자를 포함하지 않으므로', strongPassword('q1w2e3r4'))
-console.log('`q1w2e3r4!`는 조건을 충족하므로', strongPassword('q1w2e3r4!'))
+phoneNumber.onkeyup = function() {
+  if(strongPhoneNumber(phoneNumber.value)){
+    successNumber.classList.remove('hide');
+    failureNumber.classList.add('hide');
+  }else {
+    if(phoneNumber.value === ''){  // textContent니까 이게 된다
+      successNumber.classList.add('hide');
+      failureNumber.classList.add('hide');
+    }else {
+      successNumber.classList.add('hide');
+      failureNumber.classList.remove('hide');
+    }
+  }
+}
+
+// [유효성 검증 함수] : 전화 번호
+function strongPhoneNumber(num) {
+  return /\d{2,3}[- .]?\d{3,4}[- .]?\d{3,4}/.test(num);  // ?!?
+}
