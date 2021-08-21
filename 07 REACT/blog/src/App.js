dc8357db833e96ea4b5004e519e4d89512db6f9c
ciocio97 <tmddusleo@icloud.com>
2021-08-21 17:41:30 +0900

    useState, useRef

--- 07 REACT/blog/src/App.js
+++ 07 REACT/blog/src/App.js
@@ -1,66 +1,74 @@
 /* eslint-disable */ // 우와아 노란색글씨 다 없애줌 ~~
 
-import React , { useState }from "react";
+import React , { useState, useRef }from "react";
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
 
-// React는 데이터바인딩이 굉장히 쉽다
+{/* input의 개수가 여러개 됐을 때, 단순히 useState 여러번 사용하고 onChange도 여러개 만들어서 구현! 할 수 있다 */}
+{/* 그치만, 더 좋은 방법은 01 input에 name을 설정하고, 02 이벤트가 발생했을 때 이 값을 참조하는 것이다. */}
+{/* 그리고 03 useState에서는 문자열이 아니라 !! 객체 형태로 상태를 관리해줘야 한다. */}
+
 
 function App() {
 
-  const [articleName, articleNameChange] = useState(["프론트엔드개발자", "자바스크립트", "리액트"]);
-  const [createdDate, chngeDate] = useState(["2021.06.23", "2021.07.11", "2021.08.03"]);
+  // const [text, setText] = useState(''); 계속 문자열로 상태를 관리해주는 실수를 범하고 있었구나
+  // input받는 값들에 이름을 붙여준다
+  // useState는 그 이름들을 (값과 함께) 객체 형태로 관리한다
+  const [input, setInput] = useState({
+    list: ''
+    /*check: false*/
+  });
 
-  const [num, plusNum] = useState(0);  // 이렇게 state변경 함수로 만들어야 재렌더링이 잘 일어난다.
-  const plusNumOne = () => plusNum(num+1);  // 그리고 원본 state는 직접 변경안됩니다.
-                                            // 때문에 보통 자료의 '복사본'을 만들어서 변경하곤 합니다.
-  // const changeName = () => articleNameChange(["백엔드개발자" ,"자바스크립트", "리액트"])
-  const changeName = () => {          // state를 수정하고 싶어서 데이터를 복사하는 건 아주 React적인 관습이다.
-//  const newArr = articleName;          근데 이렇게 "값을 공유"하면 안되고, deep copy"깊은 복사"를 해야한다.           
-    const newArr = [...articleName];  // 이것이 바로 deep copy -> 내용은 같지만 서로 독립적인 아이를 탄생시킨다.
-    newArr[0] = "백엔드개발자";
-    articleNameChange(newArr);
+  // useRef로 특정 돔 선택하기
+  // useRef()를 사용하여 Ref 객체를 만든다
+  const listInput = useRef();
+
+  // 비구조화 할당을 통해 값을 추출한다
+  const {list} = input;
+
+  const onChange = (e) => {
+    // 비구조화 할당을 통해 값을 추출한다 (객체를 추출하는 방법)
+    const {name, value} = e.target;
+    // name키에 value값을 할당한 아이 + 기존 객체 같이 담아주기
+    setInput({...input, [name]: value});
+  }
+
+  const onReset = () => {
+    setInput({
+      list: ''
+    });
+    // 버튼을 눌렀을 때 Ref의 원래값으로 focus를 옮기는 DOM API를 호출해주었다
+    listInput.current.focus();
   }
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header>
-          <nav className="navigator">
-            <div>개발이 하고싶니?</div>
-          </nav>
-        </header>
-        <main>
-          <article className="list">
-            <h3>{articleName[0]} <span onClick={ plusNumOne }> 👍 </span> {num} </h3>
-            <button onClick={ changeName }>변경</button>
-            <p>{createdDate[0]}</p>
-            <hr/>
-          </article>
-          <article className="list">
-            <h3>{articleName[1]}</h3>
-            <p>{createdDate[1]}</p>
-            <hr/>
-          </article>
-          <article className="list">
-            <h3>{articleName[2]}</h3>
-            <p>{createdDate[2]}</p>
-            <hr/>
-          </article>
-        </main>  
-        
-        {/* Switch ? 여러 Route를 감싸서 그 중 경로가 일치하는 단 하나의 라우터만 렌더링 시켜주는 역할 */}
-        {/* Switch 사용안하면 매칭되는 모든 요소를 렌더링한다 */}
-        {/* Route ? path 속성을 지정하여 해당 path에 어떤 컴포넌트를 보여주는 역할 */}
-        {/* exact ? 주어진 경로와 정확히 일치해야만 설정한 Route 컴포넌트를 보여주는 역할 */}
-        {/* exact 속성이 없으면 해당 경로와 일치하는 중복된 Route 컴포넌트를 모두 보여줘버린다ㅜ */}
-
-
+    <div className="container">
+      <div className="input">
+        <input 
+          type="text"
+          placeholder="할일을 입력하세요"
+          // input에 이름을 설정한다
+          name="list"
+          value={list}
+          onChange={onChange}
+          // 우리가 선택하고 싶은 DOM에 (아까 만들어뒀던) Ref객체를 ref값으로 설정해준다
+          ref={listInput}
+        ></input>
+        <button
+          onClick={onReset}
+        >ENTER</button>
       </div>
-    </BrowserRouter>
+      <div className="output">
+        <ul>
+          <li>
+            {list}
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
 
 
-export default App;  // 오 여기서 App 함수를 불러오는군 !
+export default App;
