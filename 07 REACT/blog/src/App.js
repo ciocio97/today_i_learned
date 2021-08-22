b679423f5b02abd3343bbc1a0a141067c02776c3
ciocio97 <tmddusleo@icloud.com>
2021-08-22 21:57:23 +0900

    update todolist useState

--- 07 REACT/blog/src/App.js
+++ 07 REACT/blog/src/App.js
@@ -3,72 +3,81 @@
 import React , { useState, useRef }from "react";
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
+import InputTodo from './InputTodo';
+import Todolist from './Todolist';
 
-{/* input의 개수가 여러개 됐을 때, 단순히 useState 여러번 사용하고 onChange도 여러개 만들어서 구현! 할 수 있다 */}
-{/* 그치만, 더 좋은 방법은 01 input에 name을 설정하고, 02 이벤트가 발생했을 때 이 값을 참조하는 것이다. */}
-{/* 그리고 03 useState에서는 문자열이 아니라 !! 객체 형태로 상태를 관리해줘야 한다. */}
 
 
 function App() {
 
-  // const [text, setText] = useState(''); 계속 문자열로 상태를 관리해주는 실수를 범하고 있었구나
-  // input받는 값들에 이름을 붙여준다
-  // useState는 그 이름들을 (값과 함께) 객체 형태로 관리한다
-  const [input, setInput] = useState({
-    list: ''
+  const [inputs, setInputs] = useState({
+    listname: '',
     /*check: false*/
   });
 
-  // useRef로 특정 돔 선택하기
-  // useRef()를 사용하여 Ref 객체를 만든다
-  const listInput = useRef();
-
-  // 비구조화 할당을 통해 값을 추출한다
-  const {list} = input;
+  const {listname} = inputs;
 
   const onChange = (e) => {
-    // 비구조화 할당을 통해 값을 추출한다 (객체를 추출하는 방법)
     const {name, value} = e.target;
-    // name키에 value값을 할당한 아이 + 기존 객체 같이 담아주기
-    setInput({...input, [name]: value});
+    setInputs({...inputs, [name]: value});
   }
 
-  const onReset = () => {
-    setInput({
-      list: ''
+  const [lists, setLists] = useState([
+    {
+      id: 0,
+      listname: ''
+    }
+  ]);
+
+  // useRef() 사용할 때 넣어준 parm값 === .current의 기본값
+  // 왜 const nextId = {current: 4} / let nextId = 4; 안되는가 ??!!
+  /**
+   * 
+   * useRef는 일반적인 자바스크립트 객체입니다 즉 heap 영역에 저장됩니다
+   * 그래서 어플리케이션이 종료되거나 가비지 컬렉팅 될 때 까지 참조할 때 마다 같은 메모리 주소를 가지게 되고
+   * 같은 메모리 주소를 가지기 때문에 === 연산이 항상 true를 반환하고, 값이 바뀌어도 리렌더링 되지 않습니다.
+   * 
+   * 하지만 함수 컴포넌트 내부에 변수를 선언한다면, 렌더링 될 때마다 값이 초기화 됩니다.
+   * 그래서 해당 방법을 지양하는 것 같습니다 :)
+   */
+  const listInput = useRef();
+  const nextId = useRef(2);
+  const onCreate = () => {
+    
+    const newList = {
+      id: nextId.current,
+      listname
+    };
+
+    setLists([...lists, newList]);
+    // setLists(lists.concat(newList));
+
+    setInputs({
+      listname: ''
     });
-    // 버튼을 눌렀을 때 Ref의 원래값으로 focus를 옮기는 DOM API를 호출해주었다
+    // 값의 수정과 삭제 모두 .current 값을 이용한다 !!
+    nextId.current += 1;
     listInput.current.focus();
   }
 
+  const onCheckEnter = (e) => {
+    if(e.key === 'Enter'){
+      onCreate()
+    }
+  };
+
   return (
-    <div className="container">
-      <div className="input">
-        <input 
-          type="text"
-          placeholder="할일을 입력하세요"
-          // input에 이름을 설정한다
-          name="list"
-          value={list}
-          onChange={onChange}
-          // 우리가 선택하고 싶은 DOM에 (아까 만들어뒀던) Ref객체를 ref값으로 설정해준다
-          ref={listInput}
-        ></input>
-        <button
-          onClick={onReset}
-        >ENTER</button>
-      </div>
-      <div className="output">
-        <ul>
-          <li>
-            {list}
-          </li>
-        </ul>
-      </div>
-    </div>
+    <main className="container">
+      <InputTodo 
+        listname={listname}
+        onChange={onChange}
+        onCreate={onCreate}
+        onCheckEnter={onCheckEnter}
+        listInput={listInput}
+      />
+      <Todolist lists={lists} />
+    </main>
   );
 }
-
-
 
 export default App;
