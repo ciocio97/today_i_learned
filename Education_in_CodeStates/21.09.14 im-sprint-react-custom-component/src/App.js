b18ccf9163808fdd6600a57148769d8d7b2b7efa
ciocio97 <tmddusleo@icloud.com>
2021-09-18 13:36:04 +0900

    make body tag & align items center

--- Education_in_CodeStates/21.09.14 im-sprint-react-custom-component/src/App.js
+++ Education_in_CodeStates/21.09.14 im-sprint-react-custom-component/src/App.js
@@ -10,31 +10,33 @@ import './app.css';
 const App = () => {
   return (
     <>
-      <div id='container'>
-        <div id='title'>React Custom Component</div>
-        <div className='box'>
-          <div className='sub_title'>Modal</div>
-          <Modal />
-        </div>
-        <div className='box'>
-          <div className='sub_title'>Toggle</div>
-          <Toggle />
-        </div>
-        <div className='box'>
-          <div className='sub_title'>Tab</div>
-          <Tab />
-        </div>
-        <div className='box'>
-          <div className='sub_title'>Tag</div>
-          <Tag />
-        </div>
-        <div className='box'>
-          <div className='sub_title'>Autocomplete</div>
-          <Autocomplete />
-        </div>
-        <div className='box'>
-          <div className='sub_title'>ClickToEdit</div>
-          <ClickToEdit />
+      <div id="body">
+        <div id='container'>
+          <div id='title'>React Custom Component</div>
+          <div>
+            <div className='sub_title' id="fixed">Modal</div>
+            <div className='box'><Modal /></div>
+          </div>
+          <div>
+            <div className='sub_title'>Toggle</div>
+            <div className='box'><Toggle /></div>
+          </div>
+          <div>
+            <div className='sub_title'>Tab</div>
+            <div className='box'><Tab /></div>
+          </div>
+          <div>
+            <div className='sub_title'>Tag</div>
+            <div className='box'><Tag /></div>
+          </div>
+          <div>
+            <div className='sub_title'>Autocomplete</div>
+            <div className='box'><Autocomplete /></div>
+          </div>
+          <div>
+            <div className='sub_title'>ClickToEdit</div>
+            <div className='box'><ClickToEdit /></div>
+          </div>
         </div>
       </div>
     </>
