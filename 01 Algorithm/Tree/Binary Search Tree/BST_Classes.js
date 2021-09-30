8fa8a50527a5a602210182c215c89c5c8d7f18b0
ciocio97 <tmddusleo@icloud.com>
2021-10-01 04:49:15 +0900

    clean code

--- 01 Algorithm/Tree/Binary Search Tree/BST_Classes.js
+++ 01 Algorithm/Tree/Binary Search Tree/BST_Classes.js
@@ -17,27 +17,3 @@ tree.root = new Node(10);
 tree.root.right = new Node(15);
 tree.root.left = new Node(7);
 tree.root.left.right = new Node(9);
-
-
-{
-  const binarySearch = function(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
-
-    while(left <= right) {
-      let middle = parseInt((left + right) / 2);
-      if(arr[middle] === target){
-        return middle;
-      }
-      
-      if(target < arr[middle]){
-        right = middle - 1;
-      }
-      else{
-        left  = middle + 1;
-      }
-    }
-
-    return -1;
-  }
-}
