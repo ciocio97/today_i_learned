c737f486dd9dfb58893ae0febdf7c760bac6e931
ciocio97 <tmddusleo@icloud.com>
2021-10-07 14:40:16 +0900

    about object code refactoring

--- Education_in_CodeStates/21.08.25 im-sprint-stringify-json/src/stringifyJSON.js
+++ Education_in_CodeStates/21.08.25 im-sprint-stringify-json/src/stringifyJSON.js
@@ -23,121 +23,105 @@
  * 4. 그냥 테스트 통과를 하고 싶으시다면, 다음과 같이 구현하면 될 거예요.
  *  const stringifyJSON = JSON.stringify;
  *
- * 하지만 이 과제의 목적은 재귀를 공부하는 것이니, 처음부터 구현해봐야겠지요?:
  */
- function stringifyJSON(obj) {
-  // obj가 숫자일 때
-  if(typeof obj === 'number'){return String(obj);}
-  // obj가 boolean일 때
-  if(typeof obj === 'boolean'){return String(obj);}
-  // obj가 null일 때
-  if(obj === null){return String(obj);}
-  // obj가 문자열일 때
-  if(typeof obj === 'string'){return `"${obj}"`;}
-  // obj가 배열일 때
-  if(Array.isArray(obj)){
-    let result = [];
-    for(let el of obj){
-      result.push(JSON.stringify(el));
+
+{
+  function stringifyJSON(obj) {
+    // obj가 숫자일 때
+    if(typeof obj === 'number'){return String(obj);}
+    // obj가 boolean일 때
+    if(typeof obj === 'boolean'){return String(obj);}
+    // obj가 null일 때
+    if(obj === null){return String(obj);}
+    // obj가 문자열일 때
+    if(typeof obj === 'string'){return `"${obj}"`;}
+    // obj가 배열일 때
+    if(Array.isArray(obj)){
+      let result = [];
+      for(let el of obj){
+        result.push(JSON.stringify(el));
+      }
+      return `[${result}]` 
     }
-    return `[${result}]` 
-  }
-  // obj가 객체일 때
-  else{
-
-    let finalStart = "{";
-    let finalLast  = "}";
-    let str = '';
-
-    for(let key in obj){
-      
-      if(typeof obj[key] === 'function' || obj[key] === undefined){
-        continue;
-      }
-  
-      str += ('\"' + key +'\"');
-      str += ':';
-      if(typeof obj[key] === 'boolean' || obj[key] === null){
-        str += obj[key];
-      }
-      else if(Array.isArray(obj[key]) || typeof obj[key] === 'object'){
-        str += stringifyJSON(obj[key]);
-      }
-      else{
-        str += ('\"' + obj[key] +'\"');
-      }
-      str += ',';
-        
-    }
-    return finalStart + str.slice(0, -1) + finalLast;
-  }
-
-};
-
-// 다음 코드는 결과 제출을 위한 코드입니다. 신경 쓰지 않아도 좋습니다.
-if (typeof window === "undefined") {
-  module.exports = stringifyJSON;
-}
-
-/*if(Array.isArray(obj[key]) || typeof obj[key] === 'object'){
-  final[`"${key}"`] = stringifyJSON(obj[key]);
-}else if(typeof obj[key] === 'boolean' || obj[key] === null){
-  final[`"${key}"`] = obj[key];
-}else if(typeof obj[key] === 'string'){
-  final[`"${key}"`] = `${obj[key]}`;
-}*/
-
-/**
- * function stringifyJSON(obj) {
-  // obj가 숫자일 때
-  if(typeof obj === 'number'){return String(obj);}
-  // obj가 boolean일 때
-  if(typeof obj === 'boolean'){return String(obj);}
-  // obj가 null일 때
-  if(obj === null){return String(obj);}
-  // obj가 문자열일 때
-  if(typeof obj === 'string'){return `"${obj}"`;}
-  // obj가 배열일 때
-  if(Array.isArray(obj)){
-    let result = [];
-    for(let el of obj){
-      result.push(JSON.stringify(el));
-    }
-    return `[${result}]` 
-  }
-  // obj가 객체일 때
-  else{
-    for(let key in obj){
+    // obj가 객체일 때
+    else{
 
       let finalStart = "{";
       let finalLast  = "}";
       let str = '';
-      
-      if(typeof obj[key] === 'function' || obj[key] === undefined){
-        return '{}';
-      }
-      else{
-  
-        for(let key in obj){
 
-          str += ('\"' + key +'\"');
-          str += ':';
-          if(typeof obj[key] === 'boolean' || obj[key] === null){
-            str += obj[key];
-          }
-          else if(Array.isArray(obj[key]) || typeof obj[key] === 'object'){
-            str += stringifyJSON(obj[key]);
-          }
-          else{
-            str += ('\"' + obj[key] +'\"');
-          }
-          str += ',';
+      for(let key in obj){
         
+        if(typeof obj[key] === 'function' || obj[key] === undefined){
+          continue;
         }
+    
+        str += ('\"' + key +'\"');
+        str += ':';
+        if(typeof obj[key] === 'boolean' || obj[key] === null){
+          str += obj[key];
+        }
+        else if(Array.isArray(obj[key]) || typeof obj[key] === 'object'){
+          str += stringifyJSON(obj[key]);
+        }
+        else{
+          str += ('\"' + obj[key] +'\"');
+        }
+        str += ',';
+          
       }
       return finalStart + str.slice(0, -1) + finalLast;
     }
+
+  };
+
+  // 다음 코드는 결과 제출을 위한 코드입니다. 신경 쓰지 않아도 좋습니다.
+  if (typeof window === "undefined") {
+    module.exports = stringifyJSON;
   }
 
-};
- */
+}
+
+/* Code Refactoring */
+/* 재귀 함수 사용을 목적으로 코드 리팩토링 */
+{
+  function stringifyJSON(obj) {
+    
+    if(obj === null) return `${obj}`;
+    if(typeof obj === 'number') return `${obj}`;
+    if(typeof obj === 'boolean') return `${obj}`;
+    if(typeof obj === 'string') return `"${obj}"`;
+    // obj가 배열일 때
+    if(Array.isArray(obj)){
+      let array = [];
+
+      for(let el of obj){
+        if(typeof el !== 'number'){
+          el = stringifyJSON(el);
+          array.push(el);
+        }
+        else array.push(el);
+      }
+
+      return `[${array}]`;
+    }
+    // obj가 객체일 때
+    if(typeof obj === 'object'){
+      let object = [];
+
+      for(let key in obj){
+        if(obj[key] === undefined) continue;
+        if(typeof obj[key] === 'function') continue;
+        else object.push(stringifyJSON(key)+':'+stringifyJSON(obj[key])); 
+      }
+
+      return `{${object}}`;
+    }
+
+  };
+
+  if (typeof window === "undefined") {
+    module.exports = stringifyJSON;
+  }
+
+}
