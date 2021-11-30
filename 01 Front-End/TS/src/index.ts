f53746152990f7ec4b76f0c261c32c2892c07455
ciocio97 <tmddusleo@icloud.com>
2021-11-30 23:28:21 +0900

    studying Class by creating blockchain

--- 01 Front-End/TS/src/index.ts
+++ 01 Front-End/TS/src/index.ts
@@ -55,4 +55,30 @@
 
 {
   // Blockchain 만들면서 감 익히기
+  class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+      index: number,
+      hash: string,
+      previousHash: string,
+      data: string,
+      timestamp: number
+    ){
+      this.index = index;
+      this.hash = hash;
+      this.previousHash = previousHash;
+      this.data = data;
+      this.timestamp = timestamp;
+    }
+  }
+
+  const genesisBlock:Block = new Block(0, "20211130", "", "Hello", 1234);
+
+  const blockChain:[Block] = [genesisBlock];
+
+  console.log(blockChain);
 }
