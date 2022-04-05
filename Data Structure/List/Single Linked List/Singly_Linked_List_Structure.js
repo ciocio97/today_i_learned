51f3befdcf5d0b7fbb1ca37721caaee1d79075d4
ciocio <tmddusleo@icloud.com>
2022-04-05 23:26:57 +0900

    add : insert function

--- Data Structure/List/Single Linked List/Singly_Linked_List_Structure.js
+++ Data Structure/List/Single Linked List/Singly_Linked_List_Structure.js
@@ -96,6 +96,25 @@ class SinglyLinkedList{
         }
         return false;
     }
+    // Array의 index 삽입 기능 추가
+    insert(index, val){
+        if(index < 0) return false;
+        if(index > this.length) return false;
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+        
+        let newNode = new Node(val);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length++;
+        return true;
+        // 개인적으로 많이 놀랐던 부분 !
+        // index 이후의 값들은 for문을 돌면서 모두 교체해줘야겠다고 생각했는데,
+        // 오히려 index 이전 값과 현재 값의 자리를 바꿈으로서 한방에 해결. 
+        // ( List라 index개념이 없어서 가능한 일이기도 함. Array였으면 삽있했을 때 모든 index 교체해줘야함. )
+    }
 }
 
 var list = new SinglyLinkedList();
